72d01f64143a5c5142459025eeef35d3b97383c3
Marius-Supercode <marius.supercode@gmail.com>
2022-11-16 14:07:02 +0100

    Clock working

--- Aufgaben/Lev1_2 Date /assets/js/main.js
+++ Aufgaben/Lev1_2 Date /assets/js/main.js
@@ -33,21 +33,19 @@ let wochenTage = [
 ];
 
 
-let date1 = new Date("Februar 05, 2021 21:05:13");
+setInterval(() => {
+    let date1 = new Date("Februar 05, 2021 21:05:13");
 
-console.log(date1.getFullYear());
-console.log(date1.getMonth() + " Monat");
-console.log(date1.getDay() + " Tag");
-console.log(date1.getHours() + " Stunden");
-console.log(date1.getMinutes() + " Minuten");
-console.log(wochenTag[date1.getDay()]);
-console.log(monate[date1.getMonth()]);
+    console.log(date1.getFullYear());
+    console.log(date1.getMonth() + " Monat");
+    console.log(date1.getDay() + " Tag");
+    console.log(date1.getHours() + " Stunden");
+    console.log(date1.getMinutes() + " Minuten");
+    console.log(wochenTag[date1.getDay()]);
+    console.log(monate[date1.getMonth()]);
 
-let date = new Date();
+    let date = new Date();
+    let output = document.getElementById("clock");
+    output.innerHTML = `${wochenTage[date.getDay()]} ${date.toLocaleTimeString("en-US")} `;
+}, 1000);
 
-
-let output = document.getElementById("clock");
-
-let period = date.getHours() <= 12 ? "AM" : "PM";
-
-output.innerHTML = `${wochenTage[date.getDay()]} ${date.getUTCHours() + 1} : ${date.getMinutes()} : ${date.getSeconds()} ${period}`;
