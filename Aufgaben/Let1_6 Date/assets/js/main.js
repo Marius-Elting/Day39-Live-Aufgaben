0c1789b402123e5a926915748d60e5e166c1f23b
Marius-Supercode <marius.supercode@gmail.com>
2022-11-16 13:56:03 +0100

    changed to function

--- Aufgaben/Let1_6 Date/assets/js/main.js
+++ Aufgaben/Let1_6 Date/assets/js/main.js
@@ -1,20 +1,10 @@
-let date = new Date();
+function AmPm(date) {
+    let fullDate = new Date(date);
+    let ampm = fullDate.getHours() <= 12 ? "AM" : "PM";
+    console.log(ampm);
+}
 
-
-
-
-let date1 = new Date(1999, 10, 5, 15); //PM
-let date2 = new Date();
-let date3 = new Date(2019, 12, 3, 12); //AM
-let date4 = new Date(2000, 1, 1, 11); //AM
-
-
-
-ampm = date1.getHours() <= 12 ? "AM" : "PM";
-console.log(ampm);
-ampm = date2.getHours() <= 12 ? "AM" : "PM";
-console.log(ampm);
-ampm = date3.getHours() <= 12 ? "AM" : "PM";
-console.log(ampm);
-ampm = date4.getHours() <= 12 ? "AM" : "PM";
-console.log(ampm);
+AmPm(1999, 10, 5, 15); //PM
+AmPm();
+AmPm(2019, 12, 3, 12); //AM
+AmPm(2000, 1, 1, 11); 
