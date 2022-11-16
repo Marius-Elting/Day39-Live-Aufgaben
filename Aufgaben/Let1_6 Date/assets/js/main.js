8882f6aaab7cf2bce9717f28851fa6a3f54b6140
Marius-Supercode <marius.supercode@gmail.com>
2022-11-16 14:10:21 +0100

    Clock working

--- Aufgaben/Let1_6 Date/assets/js/main.js
+++ Aufgaben/Let1_6 Date/assets/js/main.js
@@ -1,10 +1,10 @@
 function AmPm(date) {
     let fullDate = new Date(date);
-    let ampm = fullDate.getHours() <= 12 ? "AM" : "PM";
+    let ampm = fullDate.getHours() < 12 ? "AM" : "PM";
     console.log(ampm);
 }
 
-AmPm(1999, 10, 5, 15); //PM
+AmPm(1999, 10, 5, 15);
 AmPm();
-AmPm(2019, 12, 3, 12); //AM
+AmPm(2019, 12, 3, 12);
 AmPm(2000, 1, 1, 11); 
